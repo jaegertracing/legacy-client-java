3a25fae8826aea7d03f60a485e1e36385d00de68
Yuri Shkuro <ys@uber.com>
2018-04-17 17:32:33 -0400

    Prepare fake properties file
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- travis/prepare-signing.sh
+++ travis/prepare-signing.sh
@@ -3,6 +3,14 @@
 ENCRYPTION_KEY=$1
 ENCRYPTION_IV=$2
 
+# prepare fake file first, in case other checks fail
+rm -rf "$HOME/.gradle/gradle.properties"
+echo signing.keyId="invalid-secret" > "$HOME/.gradle/gradle.properties"
+echo signing.password="invalid-secret" >> "$HOME/.gradle/gradle.properties"
+echo ossrhUsername="invalid-secret" >> "$HOME/.gradle/gradle.properties"
+echo ossrhPassword="invalid-secret" >> "$HOME/.gradle/gradle.properties"
+echo signing.secretKeyRingFile="${HOME}/.gnupg/secring.gpg" >> "$HOME/.gradle/gradle.properties"
+
 # some sanity checks first:
 if [ "x${ENCRYPTION_KEY}" == "x" ]; then
     echo "The encryption key has not been provided. Skipping signing preparation"
@@ -29,12 +37,6 @@ if [ "${TRAVIS_SECURE_ENV_VARS}" == true ]; then
     echo ossrhPassword="${SONATYPE_PASSWORD}" >> "$HOME/.gradle/gradle.properties"
     echo signing.secretKeyRingFile="${HOME}/.gnupg/secring.gpg" >> "$HOME/.gradle/gradle.properties"
 else
-    rm -rf "$HOME/.gradle/gradle.properties"
-    echo signing.keyId="invalid-secret" > "$HOME/.gradle/gradle.properties"
-    echo signing.password="invalid-secret" >> "$HOME/.gradle/gradle.properties"
-    echo ossrhUsername="invalid-secret" >> "$HOME/.gradle/gradle.properties"
-    echo ossrhPassword="invalid-secret" >> "$HOME/.gradle/gradle.properties"
-    echo signing.secretKeyRingFile="${HOME}/.gnupg/secring.gpg" >> "$HOME/.gradle/gradle.properties"
     echo "Travis secure env vars not set. Skipping."
     exit 0
 fi
